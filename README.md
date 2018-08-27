0be068b26b1365697bb1753f7be842ac5d318263
Abel Mendez <abel.mendez@upr.edu>
2018-08-26 21:14:36 -0400

    Averages for elliptic orbits

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# PHL Library
+The Planetary Habitability Library
+
+This Python module contains a collection of functions related to planetary habitability.
+This is a work in progress and the first version is planned for December 2018.
+
+Functions (references in brackets):
+
+	teq(a,e,A,L) - orbital average equilibrium temperature (K) [1]
+	dist(a,e) - orbital average distance (AU) [1]
+	flux(a,e,L) - orbital average stellar flux (solar units) [1]
+	reff(a,e) - orbital effective thermal distance (AU) [1]
+
+Definitions:
+	
+	a - orbital semi-major axis (AU)
+	e - orbital eccentricity
+	A - planetary bond albedo
+	L - stellar luminosity (solar units)
+
+Usage:
+
+	Copy phl.py to your Python working directory. 
+	
+	Example:
+	
+	In : import phl
+	In : phl.teq(1.0, 0.0, 0.3, 1.0)
+	Out: 254.74150455519143
+	
+Requirements:
+	Requires the Math and SciPy Python libraries.
+
+References:
+
+[1]	Méndez, Abel, and Edgard G. Rivera-Valentín. “The Equilibrium Temperature of
+	Planets in Elliptical Orbits.” The Astrophysical Journal Letters 837, no. 1 (2017):
+	L1. https://doi.org/10.3847/2041-8213/aa5f13.
---
+++ phl.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""
+
+The Planetary Habitability Library
+
+This Python module contains a collection of functions related to planetary habitability.
+This is a work in progress and the first version is planned for December 2018.
+
+Example:
+	In : import phl
+	In : phl.teq(1.0,0.0,0.3,1.0)
+	Out: 254.74150455519143
+
+References:
+
+[1]	Méndez, Abel, and Edgard G. Rivera-Valentín. “The Equilibrium Temperature of
+	Planets in Elliptical Orbits.” The Astrophysical Journal Letters 837, no. 1 (2017):
+	L1. https://doi.org/10.3847/2041-8213/aa5f13.
+
+"""
+
+__author__ = "Abel Mendez"
+__copyright__ = "PHL (2018)"
+__credits__ = "PHL @ UPR Arecibo"
+__license__ = "GPL"
+__version__ = "0.0"
+__maintainer__ = "Abel Mendez"
+__email__ = "abel.mendez@upr.edu"
+__status__ = "Production"
+
+import math
+from scipy import special
+
+# orbital average distance [1]
+# a = semi-major axis (AU), e = eccentricity
+def dist(a,e):
+	return a*(1+(e**2)/2)
+	
+# orbital average stellar flux [1]
+# a = semi-major axis (AU), e = eccentricity, L = luminosity (solar units)
+def flux(a,e,L):
+	return L/(a**2*math.sqrt(1 - e**2))
+	
+# orbital average equilibrium temperature [1]
+# a = semi-major axis (AU), e = eccentricity, A = albedo, L = luminosity (solar units)
+def teq(a,e,A,L):
+	To = 278.5
+	return To*((1-A)*L/a**2)**(1/4.)*(2*math.sqrt(1+e)/math.pi)*\
+	special.ellipe(math.sqrt(2*e/(1+e)))
+	
+# effective thermal distance [1]
+# a = semi-major axis (AU), e = eccentricity
+def reff(a,e):
+	return a*((2*math.sqrt(1+e)/math.pi)*special.ellipe(math.sqrt(2*e/(1+e))))**(-2.)
