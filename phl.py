10c441669240da626fea25dc78de87f7cd692497
peterleroy <42743321+peterleroy@users.noreply.github.com>
2018-12-11 10:58:14 -0400

    Update phl.py

--- phl.py
+++ phl.py
@@ -18,6 +18,7 @@ References:
 	L1. https://doi.org/10.3847/2041-8213/aa5f13.
 
 """
+from typing import Any, Union
 
 __author__ = "Abel Mendez"
 __copyright__ = "PHL (2018)"
@@ -31,24 +32,86 @@ __status__ = "Production"
 import math
 from scipy import special
 
+
 # orbital average distance [1]
 # a = semi-major axis (AU), e = eccentricity
-def dist(a,e):
-	return a*(1+(e**2)/2)
-	
+def dist(a, e):
+    return a * (1 + (e ** 2) / 2)
+
+
 # orbital average stellar flux [1]
 # a = semi-major axis (AU), e = eccentricity, L = luminosity (solar units)
-def flux(a,e,L):
-	return L/(a**2*math.sqrt(1 - e**2))
-	
+def flux(a, e, L):
+    return L / (a ** 2 * math.sqrt(1 - e ** 2))
+
+
 # orbital average equilibrium temperature [1]
 # a = semi-major axis (AU), e = eccentricity, A = albedo, L = luminosity (solar units)
-def teq(a,e,A,L):
-	To = 278.5
-	return To*((1-A)*L/a**2)**(1/4.)*(2*math.sqrt(1+e)/math.pi)*\
-	special.ellipe(2*e/(1+e))
-	
+def teq(a, e, A, L):
+    return 278.5 * ((1 - A) * L / a ** 2) ** (1 / 4.) * (2 * math.sqrt(1 + e) / math.pi) * \
+           special.ellipe(2 * e / (1 + e))
+
+
 # effective thermal distance [1]
 # a = semi-major axis (AU), e = eccentricity
-def reff(a,e):
-	return a*((2*math.sqrt(1+e)/math.pi)*special.ellipe(2*e/(1+e)))**(-2.)
+def reff(a, e):
+    return a * ((2 * math.sqrt(1 + e) / math.pi) * scipy.special.ellipe(2 * e / (1 + e))) ** (-2.)
+
+
+def edist(a, e, Eo):
+    return a * ((((e ** 2) + 2) * Eo + e * (e * math.cos(Eo - 4)) * (
+        math.sin(Eo))) / (2 * (Eo - e * math.sin(Eo))))
+
+
+def eflux(a, e, L, Eo):
+    return (L / a ** 2) * ((2 * math.atan(math.sqrt(1 + e / 1 - e)) * (math.tan(1 / 2) * Eo)) / (
+            (Eo - math.sin(e) * Eo) * math.sqrt(1 - e ** 2)))
+
+
+def eteq(a, e, A, L, Eo):
+    return 278.5 * ((L * (1 - A) / a ** 2) ** (1 / 4)) * ((2 * 1j * math.sqrt(1 - e)) / (Eo - e * math.sin(Eo))) * (
+            special.ellipkinc((1 + e) / (1 - e), 1j * math.asinh(math.tan(1 / 2) * Eo)) - special.ellipeinc(
+        (1 + e) / (1 - e), 1j * math.asinh(math.tan(1 / 2) * Eo)) - 1j * math.sqrt((1 - e * math.cos(Eo)) / (1 - e)) *
+            math.tan(1 / 2) * Eo)
+
+
+def preff(Do, rp):
+    return rp((3 * Do ** 4 + 10 * Do ** 2 + 15) / (5 * (Do ** 2 + 3)))
+
+
+def pflux(L, rp):
+    return (L / rp ** 2) * ((3 * math.atan(Do)) / ((Do ** 2 + 3) * Do))
+
+
+def pteq(A, L, Do):
+    return 278.5 * (((L * (1 - A)) / (rp ** 2)) ** (1 / 4)) * (
+            (3 * (Do * math.sqrt(Do ** 2 + 1) + math.asinh(Do))) / (2 * (Do ** 2 + 3) * Do))
+
+
+def hreff(e, Ho):
+    return rp * ((e * (e * math.cosh(Ho) - 4) * math.sinh(Ho) + ((e ** 2) + 2) * Ho) / (
+            2 * (e * math.sinh(Ho) - Ho) * (e - 1)))
+
+
+def hflux(e, L, Ho, rp):
+    return (L / rp ** 2) * ((2 * ((e - 1) ** 2) * (((e ** 2) - 1) ** (1 / 2)) / (e * math.sinh(Ho) - Ho)) * math.atan(
+        ((e + 1) * math.tanh(1 / 2) * Ho) / (math.sqrt((e ** 2) - 1))))
+
+
+def hteq(e, A, L, Ho):
+    return 278.5 * ((L * (1 - A)) ** (1 / 4)) * (
+            ((-2 * math.sqrt(-1) * (e - 1)) / (e * math.sinh(Ho) - Ho)) * special.ellipkinc(
+        (1 / 2) * math.sqrt(-1) * Ho, ((2 * e) / (e - 1))))
+
+
+def lreff(rp, Lo):
+    return rp * ((math.sinh(2 * Lo) + 2 * Lo) / (4 * math.sinh(Lo)))
+
+
+def lflux(L, rp, Lo):
+    return (L / rp ** 2) * (2 * math.atan(math.tanh((1 / 2) * Lo)) / math.sinh(Lo))
+
+
+def lteq(A, L, Lo, rp):
+    return 278.5 * ((L * (1 - A) / rp ** 2) ** (1 / 4)) * (
+            -2j / math.sinh(Lo) * special.ellipkinc((1 / 2) * 1j * Lo, 2))
